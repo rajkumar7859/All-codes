7b5411e1fa701c84f3721235057ca31762be6f12
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2022-09-04 15:54:22 +0530

    userProfile with context

--- All React code/UserProfile_with_Mode/src/App.js
+++ All React code/UserProfile_with_Mode/src/App.js
@@ -1,10 +1,7 @@
-import { useContext } from "react";
 import UserProfile from "./Components/UserProfile";
-// import { ThemeContextProvider } from "./Context/ThemeContext";
 import "./styles.css";
 
 export default function App() {
-  // const { isTheme } = useContext(ThemeContextProvider);
 
   return (
     <div className={`App`}>
