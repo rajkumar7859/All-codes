fe78b342ade447761fd513ef2205c29f67b6c66a
rajkumar7859 <rajkumarvishwakarma8759@gmail.com>
2022-11-04 01:42:12 +0530

    df

--- React-redux&TypeScript/typeScriptDay_2/assignment_1/src/Components/TodoInput.tsx
+++ React-redux&TypeScript/typeScriptDay_2/assignment_1/src/Components/TodoInput.tsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
-const TodoInput = () => {
+type TodoOnAdd = () => {
+  onAdd: Function;
+};
+
+const TodoInput = (props: TodoOnAdd) => {
+  const { onAdd } = props;
+  const [value, setValue] = useState<string>("");
+
+  const handleChange = (event: ChangeEvent<HTMLFormElement>) => {
+    setValue(event.target.value);
+  };
+  const handleOnSubmitForm = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    onAdd(value);
+    setValue("");
+  };
   return (
     <div>
-      <h1>Todo Item</h1>
+      <form onSubmit={handleOnSubmitForm}>
+        <input
+          value={value}
+          placeholder="     write somthing here"
+          type="text"
+          onChange={handleChange}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default TodoInput
+export default TodoInput;
